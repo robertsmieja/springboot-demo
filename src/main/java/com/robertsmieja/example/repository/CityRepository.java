e9436ed2a038570743606fca20f39d086376c06f
Robert Smieja <robertsmieja@gmail.com>
2016-12-10 00:59:43 -0500

    Add JDBC stuff

---
+++ src/main/java/com/robertsmieja/example/repository/CityRepository.java
@@ -0,0 +1,9 @@
+package com.robertsmieja.example.repository;
+
+import com.robertsmieja.example.domain.City;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CityRepository extends PagingAndSortingRepository<City, Long> {
+}
