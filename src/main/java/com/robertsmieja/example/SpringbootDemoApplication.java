df6dda73568c3140f236b4a318832167e6d7f9a4
Robert Smieja <robertsmieja@gmail.com>
2016-12-12 03:37:42 -0500

    Revert "WIP for configuration step"
    This reverts commit 0de0e7bca33423537591c55b1bb30d58cf6ce599.

--- src/main/java/com/robertsmieja/example/SpringbootDemoApplication.java
+++ src/main/java/com/robertsmieja/example/SpringbootDemoApplication.java
@@ -1,9 +1,9 @@
 package com.robertsmieja.example;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,21 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringbootDemoApplication {
 
-    @Value("${hello.world}")
-    String world;
-
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello " + world + "!";
+        return "Hello World!";
     }
 
     public static void main(String[] args) {
-        //For more configuration options, we use the builder
-        SpringApplicationBuilder builder = new SpringApplicationBuilder(SpringbootDemoApplication.class);
-
-        builder
-                .profiles("development") //set dev as default
-                .run(args);
+        SpringApplication.run(SpringbootDemoApplication.class, args);
     }
 }
