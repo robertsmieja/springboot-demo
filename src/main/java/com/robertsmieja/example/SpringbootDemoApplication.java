bdc6409f0ca730d16238bb76daa21d5becc18672
Robert Smieja <robertsmieja@gmail.com>
2016-12-11 23:35:37 -0500

    Strip application down to step 1

--- src/main/java/com/robertsmieja/example/SpringbootDemoApplication.java
+++ src/main/java/com/robertsmieja/example/SpringbootDemoApplication.java
@@ -3,10 +3,21 @@ package com.robertsmieja.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@Controller
 public class SpringbootDemoApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
     }
--- src/main/java/com/robertsmieja/example/repository/CityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.robertsmieja.example.repository;
-
-import com.robertsmieja.example.domain.City;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CityRepository extends PagingAndSortingRepository<City, Long> {
-}
