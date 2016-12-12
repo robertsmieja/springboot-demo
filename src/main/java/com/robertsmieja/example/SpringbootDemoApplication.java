cb5b661992752820c8c3d10c0a0364dbecf3414c
Robert Smieja <robertsmieja@gmail.com>
2016-12-12 00:27:50 -0500

    Change @Controller to @RestController

--- src/main/java/com/robertsmieja/example/SpringbootDemoApplication.java
+++ src/main/java/com/robertsmieja/example/SpringbootDemoApplication.java
@@ -6,10 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@Controller
+@RestController
 public class SpringbootDemoApplication {
 
     @RequestMapping("/")
