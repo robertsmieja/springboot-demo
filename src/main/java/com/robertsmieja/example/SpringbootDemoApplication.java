4f1256c6aaf82822ce189e85593f81c2d7bfb055
Robert Smieja <robertsmieja@gmail.com>
2016-12-13 04:42:02 -0500

    Remove excess code/comments for this step

--- src/main/java/com/robertsmieja/example/SpringbootDemoApplication.java
+++ src/main/java/com/robertsmieja/example/SpringbootDemoApplication.java
@@ -1,13 +1,33 @@
 package com.robertsmieja.example;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@RestController
 public class SpringbootDemoApplication {
+
+    @Value("${hello.world}")
+    String world;
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello " + world + "!";
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootDemoApplication.class, args);
+        //For more configuration options, we use the builder
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(SpringbootDemoApplication.class);
+
+        builder
+                .profiles("development") //set dev as default
+                .run(args);
     }
 }
